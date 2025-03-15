4a75363a242862cd1471391a1b529d08d4265056
grez72 <grez72@gmail.com>
2025-03-15 15:51:04 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='remote_data',
+    version='0.1.0',
+    packages=['visionlab.remote_data'],
+    package_dir={'visionlab.remote_data': 'remote_data'},
+    python_requires='>=3.3',
+    # Other metadata such as classifiers, description, etc.
+)
